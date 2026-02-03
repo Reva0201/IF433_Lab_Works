ad6cdbff789e49dc5550c770eeb2a4ef80d910ad
Reva0201 <Meccapareva@gmail.com>
2026-02-03 15:44:30 +0700

    week01 : (tugas 1) add legacy CircleCalculator code

---
+++ src/oop_113059_MuhammadMeccapareva/week01/CircleCalculator.kt
@@ -0,0 +1,22 @@
+package oop_113059_MuhammadMeccapareva.week01
+
+import java.awt.geom.Area
+
+fun main(args: Array<String>) {
+    var radius: Double = 7.0
+    var pi: Double = 3.14
+
+    var area: Double = pi * radius * radius
+
+    println("Radius: " + radius + ", Area " + area)
+
+    checkSize(area)
+}
+
+fun checkSize(area: Double) {
+    if (area > 100) {
+        println("This is a Big Circle")
+    } else {
+        println("This is a Small Circle")
+    }
+}
