097d8d2499560cb1d80acf99666805a265a63046
Reva0201 <Meccapareva@gmail.com>
2026-02-03 15:28:23 +0700

    week01 : fix the file name

---
+++ src/oop_113059_MuhammadMeccapareva/week01/GradingSystem.kt
@@ -0,0 +1,20 @@
+package oop_113059_MuhammadMeccapareva.week01
+
+fun main() {
+    val name = "Tasya"
+    val score = 80
+
+    println("Nama: $name, Nilai: $score")
+
+    val grade = when (score) {
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "D"
+    }
+
+    println("Grade kamu: $grade")
+    println("Status: ${calculateStatus(score)}")
+}
+
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
