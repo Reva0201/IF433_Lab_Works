a9b8099a9f7c7ae6899d1cdf5a7642c04febb634
Reva0201 <Meccapareva@gmail.com>
2026-02-09 21:46:32 +0700

    week01 (tugas 2) setup GameStore file

--- src/oop_113059_MuhammadMeccapareva/week01/GameStore.kt
+++ src/oop_113059_MuhammadMeccapareva/week01/GameStore.kt
@@ -1,1 +1,9 @@
-package oop_113059_MuhammadMeccapareva.
+package oop_113059_MuhammadMeccapareva.week01
+
+fun main() {
+    val gameTitle = "Elden Ring"
+    val price = 799000
+
+    println("Game Title: $gameTitle")
+    println("Original Price: $price")
+}
