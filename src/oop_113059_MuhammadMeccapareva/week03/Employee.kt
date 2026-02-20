9054338f4a46a6f039dd36ac4dc8355d5b4e2899
Reva0201 <Meccapareva@gmail.com>
2026-02-20 14:54:43 +0700

    test all encapsulation logic in main

--- src/oop_113059_MuhammadMeccapareva/week03/Employee.kt
+++ src/oop_113059_MuhammadMeccapareva/week03/Employee.kt
@@ -1,23 +1,29 @@
 package oop_113059_MuhammadMeccapareva.week03
 
 class Employee(val name: String) {
+
     var salary: Int = 0
         set(value) {
-            if (value < 0)
-                println("ERROR: Gaji tidak boleh negatif! Di set ke 0.")
-            else
+            if (value < 0) {
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+                field = 0
+            } else {
                 field = value
+            }
         }
 
     private var performanceRating: Int = 3
 
-    fun increasePerformanceRating() {
-        performanceRating ++
+    fun increasePerformance() {
+        performanceRating++
         println("Kinerja $name meningkat! Rating: $performanceRating")
     }
 
     fun printStatus() {
-        println("Karyawan $name, Rating: $performanceRating")
+        println("Karyawan: $name, Rating: $performanceRating")
     }
+
+    val tax: Double
+        get() = salary * 0.1
 }
 
