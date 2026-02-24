a75ec45137015561164e3573bab4351793c95017
Reva0201 <Meccapareva@gmail.com>
2026-02-24 15:12:02 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_113059_MuhammadMeccapareva/week04/Main.kt
+++ src/oop_113059_MuhammadMeccapareva/week04/Main.kt
@@ -11,4 +11,10 @@ fun main() {
     myCar.openTrunk()
     myCar.honk()
     myCar.accelerate()
+
+    println("\n--- Testing ElectricCar ---")
+    val myEC = ElectricCar(brand = "BYD", numberOfDoors = 4, batteryCapacity = 67)
+    myEC.accelerate()
+    myEC.honk()
+    myEC.openTrunk()
 }
