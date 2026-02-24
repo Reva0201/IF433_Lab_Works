972eaa9d65d491c07202613e8aaef6fdaa1b168f
Reva0201 <Meccapareva@gmail.com>
2026-02-24 14:45:11 +0700

    week04: create basic Vehicle parent class

---
+++ src/oop_113059_MuhammadMeccapareva/week04/Vehicle.kt
@@ -0,0 +1,14 @@
+package oop_113059_MuhammadMeccapareva.week04
+
+open class Vehicle {
+    var speed: Int = 0
+
+    open fun accelerate() {
+        speed += 10
+        println("$brand melaju. Kecepatan $speed km/jam")
+    }
+
+    open fun honk() {
+        println("Beep beep!")
+    }
+}
