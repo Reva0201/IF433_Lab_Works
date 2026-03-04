a625c7c4ceb285f8c5bcd9bac49f46627478f659
Reva0201 <Meccapareva@gmail.com>
2026-03-04 20:02:13 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_113059_MuhammadMeccapareva/week05/MathHelper.kt
@@ -0,0 +1,17 @@
+package oop_113059_MuhammadMeccapareva.week05
+
+class MathHelper {
+
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+
+}
