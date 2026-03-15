fc88cf0425351f14b50f1e6203d839e95124445c
Reva0201 <Meccapareva@gmail.com>
2026-03-15 21:37:38 +0700

    week06: (task) Create SmartCCTV with compund interfaces

---
+++ src/oop_113059_MuhammadMeccapareva/week06/SmartCCTV.kt
@@ -0,0 +1,20 @@
+package oop_113059_MuhammadMeccapareva.week06
+
+class SmartCCTV(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable, Recordable {
+    override fun turnOn() {
+        println("CCTV $name menyala.")
+        startRecord()
+    }
+
+    override fun turnOff() {
+        println("CCTV $name dimatikan.")
+        stopRecord()
+    }
+
+    override fun startRecord() {
+        println("CCTV $name mulai merekam.")
+    }
+}
