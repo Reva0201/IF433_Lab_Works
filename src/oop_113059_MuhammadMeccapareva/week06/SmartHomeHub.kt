4c2a4fe9f384b6e8df0987dce1b5382e04693439
Reva0201 <Meccapareva@gmail.com>
2026-03-15 21:38:54 +0700

    week06: (task) Create SmartHomeHub logic

---
+++ src/oop_113059_MuhammadMeccapareva/week06/SmartHomeHub.kt
@@ -0,0 +1,22 @@
+package oop_113059_MuhammadMeccapareva.week06
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+        println("${device.name} berhasil ditambahkan ke Smart Home Hub.")
+    }
+
+    fun turnOffAllSwitches() {
+
+        for (device in devices) {
+
+            if (device is Switchable) {
+                device.turnOff()
+            }
+
+        }
+
+    }
+}
